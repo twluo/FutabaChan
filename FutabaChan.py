ae07879821ea8c228436f5baab123b14da03200f
Tony Luo <tonywluo@gmail.com>
2020-05-02 22:27:45 -0700

    Adding discord interface, gating mechanism and retry mechanism

--- FutabaChan.py
+++ FutabaChan.py
@@ -1,1 +1,37 @@
-print("hello")
+import discord
+import re
+from AUTH_KEYS import DISCORD_TOKEN
+from clients import RiotClient
+
+futaba_chan = discord.Client()
+rc = RiotClient.RiotClient()
+
+@futaba_chan.event
+async def on_ready():
+    """ Event handler for when Futaba is ready to receive messages """
+    print('Logged in as')
+    print(futaba_chan.user.name)
+    print(futaba_chan.user.id)
+    print('------')
+
+@futaba_chan.event
+async def on_message(message):
+    """ Event handler for receiving messages
+    Arguments
+    ---------
+    message : Message
+        Message received
+    """
+    if message.content.startswith('!league'):
+        channel = message.channel
+        p = re.compile("[\"][\w\s]+[\"]")
+        m = p.findall(message.content)
+        user1 = m[0].strip('"')
+        user2 = m[1].strip('"')
+        await channel.send("Finding path between %s and %s" % (user1, user2))
+        path, match_history = rc.find_shortest_distance(user1, user2)
+        print(path, match_history)
+        await channel.send(path)
+        await channel.send(match_history)
+
+futaba_chan.run(DISCORD_TOKEN)
--- clients/RiotClient.py
+++ clients/RiotClient.py
@@ -1,6 +1,8 @@
 import requests
 from requests.utils import requote_uri
-from AUTH_KEYS import RIOT_API_KEY, TEST_USER
+from AUTH_KEYS import RIOT_API_KEY
+import copy
+import time
 
 VERSION_ENDPOINT = "https://ddragon.leagueoflegends.com/realms/na.json"
 SUMMONER_SPELL_ENDPOINT = "http://ddragon.leagueoflegends.com/cdn/%s/data/en_US/summoner.json"
@@ -8,36 +10,59 @@ RIOT_ENDPOINT = "https://na1.api.riotgames.com"
 GET_SUMMONER_BY_NAME_ENDPOINT = RIOT_ENDPOINT + "/lol/summoner/v4/summoners/by-name/%s"
 GET_MATCH_LIST_BY_ACCOUNT_ENDPOINT = RIOT_ENDPOINT + "/lol/match/v4/matchlists/by-account/%s"
 GET_MATCH_INFO_BY_MATCH_ID_ENDPOINT = RIOT_ENDPOINT + "/lol/match/v4/matches/%s"
+MATCH_HISTORY = "https://matchhistory.na.leagueoflegends.com/en/#match-details/NA1/%s/%s?tab=overview"
 
-
-def get_request(url, headers=None, params=None):
-    encoded_url = requote_uri(url)
-    return requests.get(encoded_url, headers=headers, params=params)
+DELAY = 1.2
+DEBUG = 1
+RETRY_COUNT = 5
 
 
 class RiotClient:
     def __init__(self):
+        self.then_time = time.time()
         self.version = self.get_version()
         self.summoner_spell_data = self.get_summoner_spell_data()
         self.headers = {'X-Riot-Token': RIOT_API_KEY}
         self.summoner_name_map = {}
         self.match_id_map = {}
 
+    def get_version(self):
+        return self.get_request(VERSION_ENDPOINT).json()['v']
+
     @staticmethod
-    def get_version():
-        return get_request(VERSION_ENDPOINT).json()['v']
+    def check_summoner_data_equality(summoner1_data, summoner2_data):
+        return summoner1_data['summonerName'] == summoner2_data['summonerName']
+
+    def get_request(self, url, headers=None, params=None):
+        retry_attempt = 0
+        while retry_attempt <= RETRY_COUNT:
+            now_time = time.time()
+            gap = now_time - self.then_time
+            if gap < DELAY:
+                time.sleep(DELAY - gap)
+            self.then_time = time.time()
+            encoded_url = requote_uri(url)
+            request = requests.get(encoded_url, headers=headers, params=params)
+            print(request.status_code)
+            if request.status_code == 200:
+                return request
+            retry_attempt += 1
+            print("Retry # %s" % retry_attempt)
 
     def get_summoner_spell_data(self):
-        return get_request(SUMMONER_SPELL_ENDPOINT % self.version).json()
+        return self.get_request(SUMMONER_SPELL_ENDPOINT % self.version).json()
 
     def get_player_info(self, summoner_name):
         if summoner_name in self.summoner_name_map:
             return self.summoner_name_map[summoner_name]
         url = GET_SUMMONER_BY_NAME_ENDPOINT % summoner_name
-        data = get_request(url, headers=self.headers).json()
-        data_dict = {'accountId': data['accountId'], 'summonerName': summoner_name}
-        self.summoner_name_map.update({summoner_name: data_dict})
-        return data_dict
+        data = self.get_request(url, headers=self.headers)
+        if data.status_code == 200:
+            data = data.json()
+            data_dict = {'accountId': data['accountId'], 'summonerName': data['name']}
+            self.summoner_name_map.update({data['name']: data_dict})
+            return data_dict
+        return None
 
     def get_all_match_info_participants(self, summoner_name, begin_index=0, end_index=1):
         if begin_index >= end_index:
@@ -46,18 +71,21 @@ class RiotClient:
         account_id = self.get_player_info(summoner_name)['accountId']
         params = {'endIndex': end_index, 'beginIndex': begin_index}
         get_match_list_url = GET_MATCH_LIST_BY_ACCOUNT_ENDPOINT % account_id
-        matches_data = get_request(get_match_list_url, headers=self.headers, params=params).json()
+        matches_data = self.get_request(get_match_list_url, headers=self.headers, params=params).json()
         for match in matches_data['matches']:
-            match_participants = self.get_match_info_participants(match['gameId'])
+            match_id = match['gameId']
+            if DEBUG:
+                print("Checking %s" % match_id)
+            match_participants = self.get_match_info_participants(match_id)
             for participant in match_participants:
-                participants.add(participant['summonerName'])
+                participants.add((participant['summonerName'], match_id))
         return participants
 
     def get_match_info_participants(self, match_id):
         if match_id in self.match_id_map:
             return self.match_id_map[match_id]
         get_match_data_url = GET_MATCH_INFO_BY_MATCH_ID_ENDPOINT % match_id
-        match_data = get_request(get_match_data_url, headers=self.headers).json()
+        match_data = self.get_request(get_match_data_url, headers=self.headers).json()
         participants = []
         for participant in match_data['participantIdentities']:
             player = participant['player']
@@ -73,7 +101,31 @@ class RiotClient:
         self.match_id_map.update({match_id: participants})
         return participants
 
+    def find_shortest_distance(self, summoner1, summoner2):
+        summoner1_data = self.get_player_info(summoner1)
+        summoner2_data = self.get_player_info(summoner2)
+        if summoner1_data is None or summoner2_data is None:
+            return [], []
+        if self.check_summoner_data_equality(summoner1_data, summoner2_data):
+            return [summoner1_data['summonerName']], []
+        discovered_set = set(summoner1_data['summonerName'])
+        discovery_q = [(summoner1_data['summonerName'], [], [])]
+        while len(discovery_q) != 0:
+            summoner, path_so_far_, match_id_path_ = discovery_q.pop(0)
+            if DEBUG:
+                print(summoner, path_so_far_, match_id_path_)
+            if self.check_summoner_data_equality(self.get_player_info(summoner), summoner2_data):
+                path_so_far_.append(summoner)
+                match_history = [self.get_match_history_url(name, id) for name, id in zip(path_so_far_, match_id_path_)]
+                return path_so_far_, match_history
+            for participant, match_id in self.get_all_match_info_participants(summoner, end_index=10):
+                if participant not in discovered_set:
+                    path_so_far = copy.deepcopy(path_so_far_)
+                    match_id_path = copy.deepcopy(match_id_path_)
+                    match_id_path.append(match_id)
+                    path_so_far.append(summoner)
+                    discovered_set.add(participant)
+                    discovery_q.append((participant, path_so_far, match_id_path))
 
-rc = RiotClient()
-print(rc.get_all_match_info_participants(TEST_USER, end_index=2))
-
+    def get_match_history_url(self, summoner_name, match_id):
+        return MATCH_HISTORY % ( match_id, self.get_player_info(summoner_name)['matchHistoryId'])
