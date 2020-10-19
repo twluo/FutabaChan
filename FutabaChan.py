a0366adf1dc1d30fd6c1504edcee7a67cec5105e
Tony Luo <tonywluo@gmail.com>
2020-10-18 21:28:24 -0700

    Get win rate with someone implemented

--- FutabaChan.py
+++ FutabaChan.py
@@ -1,9 +1,36 @@
 import discord
-import re
 from AUTH_KEYS import DISCORD_TOKEN
 from clients import RiotClient
+import statistics
+from statistics import mode
+
+def get_combo(sum, n, repeat_flag = False):
+    # Initialize final answer
+    if repeat_flag is None:
+        repeat_flag = False
+    solutions = set()
+    def combo_helper(sum, n, path, repeat_flag):
+        if (n < 0):
+            return
+        if (n == 0 and sum == 0):
+            path.sort()
+            x = str(path)
+            solutions.add(x)
+        for i in range(1, 10):
+            new_path = [x for x in path]
+            new_path.append(i)
+            if repeat_flag:
+                if sum - 1 >= 0:
+                    combo_helper(sum - i, n - 1, new_path, repeat_flag)
+            else:
+                if sum - i >= 0 and i not in path:
+                    combo_helper(sum - i, n - 1, new_path, repeat_flag)
+    combo_helper(sum, n, [], repeat_flag)
+    return solutions
 
 futaba_chan = discord.Client()
+
+
 rc = RiotClient.RiotClient()
 
 @futaba_chan.event
@@ -14,6 +41,7 @@ async def on_ready():
     print(futaba_chan.user.id)
     print('------')
 
+
 @futaba_chan.event
 async def on_message(message):
     """ Event handler for receiving messages
@@ -22,16 +50,32 @@ async def on_message(message):
     message : Message
         Message received
     """
-    if message.content.startswith('!league'):
-        channel = message.channel
-        p = re.compile("[\"][\w\s]+[\"]")
-        m = p.findall(message.content)
-        user1 = m[0].strip('"')
-        user2 = m[1].strip('"')
-        await channel.send("Finding path between %s and %s" % (user1, user2))
-        path, match_history = rc.find_shortest_distance(user1, user2)
-        print(path, match_history)
-        await channel.send(path)
-        await channel.send(match_history)
+    channel = message.channel
+    if message.content.startswith("!get_rivalry"):
+        message_split = message.content.split()
+        protag = str(message_split[1])
+        rival = str(message_split[2])
+        history = int(message_split[3])
+        with_wins, with_losses, against_wins, against_losses, friends, rivals = rc.get_rival_record(protag, rival, 0, history)
+        total_withs = with_wins + with_losses
+        total_againts = against_wins + against_losses
+        await channel.send("You've won " + str(with_wins) + " out of " + str(total_withs) + " games together.")
+        await channel.send("You've won " + str(against_wins) + " out of " + str(total_againts) + " games against.")
+        bff, enemy = mode(friends), mode(rivals)
+        await channel.send("Your bff is " + str(bff) + " with " + str(friends.count(bff)) + " games.")
+        await channel.send("Your biggest rival is " + str(enemy) + " with " + str(rivals.count(enemy)) + " games.")
+    if message.content.startswith("!get_combo"):
+        message_split = message.content.split()
+        target = int(message_split[1])
+        n = int(message_split[2])
+        await channel.send(get_combo(target, n))
+    if message.content.startswith("!get_combo_repeat"):
+        message_split = message.content.split()
+        target = int(message_split[1])
+        n = int(message_split[2])
+        message_send = get_combo(target, n, True)
+        print(message_send)
+        await channel.send(message_send)
+
 
 futaba_chan.run(DISCORD_TOKEN)
--- clients/RiotClient.py
+++ clients/RiotClient.py
@@ -13,30 +13,28 @@ GET_MATCH_LIST_BY_ACCOUNT_ENDPOINT = RIOT_ENDPOINT + "/lol/match/v4/matchlists/b
 GET_MATCH_INFO_BY_MATCH_ID_ENDPOINT = RIOT_ENDPOINT + "/lol/match/v4/matches/%s"
 MATCH_HISTORY = "https://matchhistory.na.leagueoflegends.com/en/#match-details/NA1/%s/%s?tab=overview"
 
-DELAY = 1.2
+DELAY = 1.5
 DEBUG = 1
 RETRY_COUNT = 5
 
 
-def write_to_file(map, filename):
-    dump = json.dumps(map)
-    f = open(filename, "w")
-    f.write(dump)
-    f.close()
-
-
 class RiotClient:
+    class Player:
+        def __init__(self, account_id, summoner_name):
+            self.account_id = account_id
+            self.summoner_name = summoner_name
+
+        def __str__(self):
+            return self.summoner_name
+
+        def __repr__(self):
+            return str(self)
+
     def __init__(self):
         self.then_time = time.time()
         self.version = self.get_version()
         self.summoner_spell_data = self.get_summoner_spell_data()
         self.headers = {'X-Riot-Token': RIOT_API_KEY}
-        self.summoner_name_map = {}
-        self.match_id_map = {}
-        with open('summoner_name_map.json') as json_file:
-            self.summoner_name_map = json.load(json_file)
-        with open('match_id_map.json') as json_file:
-            self.match_id_map = json.load(json_file)
 
     def get_version(self):
         return self.get_request(VERSION_ENDPOINT).json()['v']
@@ -55,7 +53,8 @@ class RiotClient:
             self.then_time = time.time()
             encoded_url = requote_uri(url)
             request = requests.get(encoded_url, headers=headers, params=params)
-            print(request.status_code)
+            if DEBUG:
+                print(request.status_code, url)
             if request.status_code == 200:
                 return request
             retry_attempt += 1
@@ -65,23 +64,48 @@ class RiotClient:
         return self.get_request(SUMMONER_SPELL_ENDPOINT % self.version).json()
 
     def get_player_info(self, summoner_name):
-        if summoner_name in self.summoner_name_map:
-            return self.summoner_name_map[summoner_name]
         url = GET_SUMMONER_BY_NAME_ENDPOINT % summoner_name
         data = self.get_request(url, headers=self.headers)
         if data.status_code == 200:
             data = data.json()
-            data_dict = {'accountId': data['accountId'], 'summonerName': data['name']}
-            self.summoner_name_map.update({data['name']: data_dict})
-            write_to_file(self.summoner_name_map, "summoner_name_map.json")
-            return data_dict
+            return self.Player(data['accountId'], data['name'])
         return None
 
-    def get_all_match_info_participants(self, summoner_name, begin_index=0, end_index=1):
+    def get_rival_record(self, protag_name, rival_name, begin_index=0, end_index=10):
+        protag = self.get_player_info(protag_name)
+        matches_info = self.get_all_match_info_participants(protag, begin_index, end_index)
+        with_wins = 0
+        against_wins = 0
+        with_losses = 0
+        against_losses = 0
+        rivals = []
+        friends = []
+        for match in matches_info:
+            protag_win = match[protag_name]
+            if rival_name in match:
+                rival_win = match[rival_name]
+                if protag_win is True:
+                    if protag_win is rival_win:
+                        with_wins += 1
+                    else:
+                        against_wins += 1
+                else:
+                    if protag_win is rival_win:
+                        with_losses += 1
+                    else:
+                        against_losses += 1
+            for name in match:
+                if match[name] is protag_win and name != protag_name:
+                    friends.append(name)
+                elif match[name] is not protag_win:
+                    rivals.append(name)
+        return with_wins, with_losses, against_wins, against_losses, friends, rivals
+
+    def get_all_match_info_participants(self, summoner, begin_index=0, end_index=10):
         if begin_index >= end_index:
             end_index = begin_index+1
-        participants = set()
-        account_id = self.get_player_info(summoner_name)['accountId']
+        participants = []
+        account_id = summoner.account_id
         params = {'endIndex': end_index, 'beginIndex': begin_index}
         get_match_list_url = GET_MATCH_LIST_BY_ACCOUNT_ENDPOINT % account_id
         matches_data = self.get_request(get_match_list_url, headers=self.headers, params=params).json()
@@ -89,58 +113,26 @@ class RiotClient:
             match_id = match['gameId']
             if DEBUG:
                 print("Checking %s" % match_id)
-            match_participants = self.get_match_info_participants(match_id)
-            for participant in match_participants:
-                participants.add((participant['summonerName'], match_id))
+            participants.append(self.get_match_info_participants(match_id))
         return participants
 
     def get_match_info_participants(self, match_id):
-        if match_id in self.match_id_map:
-            return self.match_id_map[match_id]
+        match_id = str(match_id)
         get_match_data_url = GET_MATCH_INFO_BY_MATCH_ID_ENDPOINT % match_id
         match_data = self.get_request(get_match_data_url, headers=self.headers).json()
-        participants = []
+        participants = dict()
+        participant_team_dict = dict()
+        winners = None
+        for team in match_data['teams']:
+            if team['win'] == 'Win':
+                winners = team['teamId']
+        for participant in match_data['participants']:
+            participant_team_dict[participant['participantId']] = participant['teamId']
         for participant in match_data['participantIdentities']:
+            participant_id = participant['participantId']
             player = participant['player']
             summoner_name = player['summonerName']
             account_id = player['currentAccountId']
-            match_history_id = player['matchHistoryUri'].split('/')[-1]
-            data_dict = {'accountId': account_id, 'summonerName': summoner_name, 'matchHistoryId': match_history_id}
-            if summoner_name in self.summoner_name_map:
-                self.summoner_name_map[summoner_name] = data_dict
-            else:
-                self.summoner_name_map.update({summoner_name: data_dict})
-                write_to_file(self.summoner_name_map, "summoner_name_map.json")
-            participants.append(data_dict)
-        self.match_id_map.update({match_id: participants})
-        write_to_file(self.match_id_map, "match_id_map.json")
+            player = self.Player(account_id, summoner_name)
+            participants[player.summoner_name] = True if participant_team_dict[participant_id] == winners else False
         return participants
-
-    def find_shortest_distance(self, summoner1, summoner2):
-        summoner1_data = self.get_player_info(summoner1)
-        summoner2_data = self.get_player_info(summoner2)
-        if summoner1_data is None or summoner2_data is None:
-            return [], []
-        if self.check_summoner_data_equality(summoner1_data, summoner2_data):
-            return [summoner1_data['summonerName']], []
-        discovered_set = set(summoner1_data['summonerName'])
-        discovery_q = [(summoner1_data['summonerName'], [], [])]
-        while len(discovery_q) != 0:
-            summoner, path_so_far_, match_id_path_ = discovery_q.pop(0)
-            if DEBUG:
-                print(summoner, path_so_far_, match_id_path_)
-            if self.check_summoner_data_equality(self.get_player_info(summoner), summoner2_data):
-                path_so_far_.append(summoner)
-                match_history = [self.get_match_history_url(name, id) for name, id in zip(path_so_far_, match_id_path_)]
-                return path_so_far_, match_history
-            for participant, match_id in self.get_all_match_info_participants(summoner, end_index=10):
-                if participant not in discovered_set:
-                    path_so_far = copy.deepcopy(path_so_far_)
-                    match_id_path = copy.deepcopy(match_id_path_)
-                    match_id_path.append(match_id)
-                    path_so_far.append(summoner)
-                    discovered_set.add(participant)
-                    discovery_q.append((participant, path_so_far, match_id_path))
-
-    def get_match_history_url(self, summoner_name, match_id):
-        return MATCH_HISTORY % ( match_id, self.get_player_info(summoner_name)['matchHistoryId'])
